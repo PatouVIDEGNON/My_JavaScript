694f6f69e4fa5bdafb8d6a7d44092d44a1d5b310
PatouVIDEGNON <pvidegnon@gmail.com>
2022-04-28 17:07:37 +0200

    Select2_realise

---
+++ select2.js
@@ -0,0 +1,10 @@
+console.log('select2');
+
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.js-example-basic-single').select2();
+});
+
+$(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
+});
